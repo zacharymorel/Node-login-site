086807fa526154dd99fb2a79f86138933882a358
Zachary Morel <morelz133@gmail.com>
2017-07-27 20:25:46 -0400

    Final doc, it's done

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser')
 const mustacheExpress = require('mustache-express')
 
 app.engine('mst', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mst')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+const authenticate = (request, response, next) => {
+  if (request.body.userName === 'foob' && request.body.password === 'doob') {
+    next()
+  } else {
+    response.render('home')
+  }
+}
+
 app.get('/', (request, response) => {
   response.render('home')
 })
 
-const authenticate = (request, response, next) => {
-  if () {
+console.log(authenticate)
+app.use(authenticate)
 
-  }
-}
+app.post('/login', (request, response) => {
+  response.render('login', request.body)
+})
 
 
 
